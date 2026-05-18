fe39a098e71c3e5489c3da3bbcd0716113f9c35e
BilalShaikh33 <bilalshaikhh339@gmail.com>
2026-05-18 15:13:12 +0500

    Add files via upload

--- src/config/supabase-config.js
+++ src/config/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =  import.meta.env.VITE_SUPABASEURL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASEANON
+const supabaseUrl =  "https://cwfrjckylwfhraqdzcbm.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZnJqY2t5bHdmaHJhcWR6Y2JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODE5NDIsImV4cCI6MjA5Mzg1Nzk0Mn0.cxWeEyo6Ci-d836U2E7MV-TVoaDXCslIx196W8Ec6KM"
 
 export const supabase= createClient(supabaseUrl,supabaseAnonKey)
